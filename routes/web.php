74d5f7554792f3cdda9aafbe9ffd42cbe0ce5e64
Enrique <luiswetowkn@gmail.com>
2024-10-01 22:15:11 -0600

    Refactorizar controlador de empleados y agregar servicio para obtener todos los empleados

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Ruta de todos los empleados
+//Obtener todos los empleados (datatable)
 Route::get('/employes', [EmployeController::class, 'index'])->name('employes.index');
 
 //Registrar empleados
 Route::get('/employes/create', [EmployeController::class, 'create'])->name('employes.create');
+
+//Obtener todos los empleados
+Route::get('api/employes', [EmployeController::class, 'apiAllEmployes'])->name('employes.api');
 
 require __DIR__.'/auth.php';
